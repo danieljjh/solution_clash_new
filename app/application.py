99741edbb73b3438f5ff33fdae86f357d35de21c
jiangyiqing <alkalumia.amuy@outlook.com>
2021-12-23 21:57:06 +0800

    return data update

--- app/application.py
+++ app/application.py
@@ -5,7 +5,18 @@ from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from sclogic import Reaction
+import random
 
+p1comb = ["HCl","AgCl","CuSO4.5H2O","CaCO3","NaOH","Ca(OH)2","BaSO4","NaCl"]
+p2comb = ["HCl","AgCl","CuSO4","CaCO3","H2S","Ca(OH)2","H2SO4","NH3.H2O"]
+
+def form_card_seq(cardset):
+    cardseq = []
+    seq = random.sample(range(1,9),8)
+    for i in range(8):
+        item_dict = {"name": cardset[i],"no": seq[i]}
+        cardseq.append(item_dict)
+    return cardseq
 
 # :在这里建立了API
 app = FastAPI()
@@ -31,7 +42,12 @@ async def root():
     return {"message": "Hi cons"}
 
 
-@app.get("/generate_card_set")
-async def gen_set():
-    return ""
+@app.post("/init_cards")
+async def gen_set(): 
+    p1seq = form_card_seq(p1comb)
+    p2seq = form_card_seq(p2comb)
+    return [p1seq, p2seq]
     
+@app.post("/react")
+async def react():
+    return 
---
+++ app/functions.md
@@ -0,0 +1,17 @@
+[[{'name': 'HCl', 'no': 1}, {'name': 'AgCl', 'no': 4}, {'name': 'CuSO4.5H2O', 'no': 8}, {'name': 'CaCO3', 'no': 6}, {'name': 'NaOH', 'no': 3}, {'name': 'Ca(OH)2', 'no': 7}, {'name': 'BaSO4', 'no': 2}, {'name': 'NaCl', 'no': 5}],[]]
+
+react 的参数：
+[{
+    "score": -1,
+    "water": 7,
+    "beaker": [
+        {
+            "name": "Ca2+",
+            "amount": "2"
+        },
+        {
+            "name": "CO32-",
+            "amount": "1"
+        }
+    ]
+}]
